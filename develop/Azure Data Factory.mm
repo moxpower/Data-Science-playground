bfcfba63f67bd80cbd58034243218057c4d24db4
moxpower <maxkohl@gmx.de>
2016-06-29 08:35:12 +0200

    Cheat sheets

---
+++ develop/Azure Data Factory.mm
@@ -0,0 +1,23 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1467120963418" ID="ID_630330170" MODIFIED="1467120976583" TEXT="Azure Data Factory">
+<node CREATED="1467120977836" ID="ID_1827463547" MODIFIED="1467121036328" POSITION="right" TEXT="Fit in Cortana Analytics"/>
+<node CREATED="1467121036795" ID="ID_429689347" MODIFIED="1467121045303" POSITION="right" TEXT="ADF components"/>
+<node CREATED="1467121229020" FOLDED="true" ID="ID_255305234" MODIFIED="1467121654269" POSITION="left" TEXT="Create, orchestrate, and manage data movement and enrichement through the cloud">
+<node CREATED="1467121295403" ID="ID_651194306" MODIFIED="1467121295403" TEXT="">
+<node CREATED="1467121333031" HGAP="248" ID="ID_1238490747" MODIFIED="1467121364929" VSHIFT="-428">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="Azure Data Factory_193253321201232062.jpeg" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1467122300916" ID="ID_1324030307" MODIFIED="1467122310259" POSITION="left" TEXT="ADF Process"/>
+</node>
+</map>
