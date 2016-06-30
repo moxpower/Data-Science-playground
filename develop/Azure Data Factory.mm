e0904afba21deb7d9bd6c429c74601200d9b2d17
moxpower <maxkohl@gmx.de>
2016-06-30 16:28:42 +0200

    R

--- develop/Azure Data Factory.mm
+++ develop/Azure Data Factory.mm
@@ -1,9 +1,9 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1467120963418" ID="ID_630330170" MODIFIED="1467120976583" TEXT="Azure Data Factory">
+<node CREATED="1467120963418" ID="ID_630330170" MODIFIED="1467293966535" TEXT="Azure Data Factory">
 <node CREATED="1467120977836" ID="ID_1827463547" MODIFIED="1467121036328" POSITION="right" TEXT="Fit in Cortana Analytics"/>
-<node CREATED="1467121036795" ID="ID_429689347" MODIFIED="1467121045303" POSITION="right" TEXT="ADF components"/>
-<node CREATED="1467121229020" FOLDED="true" ID="ID_255305234" MODIFIED="1467121654269" POSITION="left" TEXT="Create, orchestrate, and manage data movement and enrichement through the cloud">
+<node CREATED="1467293966525" ID="ID_1157643385" MODIFIED="1467293969230" POSITION="left" TEXT="What">
+<node CREATED="1467121229020" FOLDED="true" ID="ID_255305234" MODIFIED="1467121654269" TEXT="Create, orchestrate, and manage data movement and enrichement through the cloud">
 <node CREATED="1467121295403" ID="ID_651194306" MODIFIED="1467121295403" TEXT="">
 <node CREATED="1467121333031" HGAP="248" ID="ID_1238490747" MODIFIED="1467121364929" VSHIFT="-428">
 <richcontent TYPE="NODE"><html>
@@ -13,11 +13,29 @@
   <body>
     <img src="Azure Data Factory_193253321201232062.jpeg" />
   </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1467122300916" ID="ID_1324030307" MODIFIED="1467293924334" POSITION="left" TEXT="ADF Process">
+<node CREATED="1467293808718" ID="ID_602643326" MODIFIED="1467293817851" TEXT="Linked Service (SQL, Hadoop Cluster)"/>
+<node CREATED="1467293818336" ID="ID_1195500521" MODIFIED="1467293828569" TEXT="Data Set (table, file)"/>
+<node CREATED="1467293828816" ID="ID_1992217590" MODIFIED="1467293850403" TEXT="Activity (hive, pig)"/>
+<node CREATED="1467293850673" ID="ID_776420594" MODIFIED="1467293866687" TEXT="Pipeline (schedule, monitor)"/>
+<node CREATED="1467293934123" ID="ID_1419816131" MODIFIED="1467293952463" TEXT="process">
+<node CREATED="1467293940051" MODIFIED="1467293940051">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="Azure Data Factory_2027020027689458971.jpeg" />
+  </body>
 </html>
 </richcontent>
 </node>
 </node>
 </node>
-<node CREATED="1467122300916" ID="ID_1324030307" MODIFIED="1467122310259" POSITION="left" TEXT="ADF Process"/>
 </node>
 </map>
